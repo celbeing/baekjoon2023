07fbd7162eddc62e7fa7074c8c0a1c105c814b0e
celbeing <kimsd1983@naver.com>
2023-02-11 17:27:17 +0900

    23.02.11.
    경사로를 ++로 쌓아서 2넘는게 있다면 거르는 게 깔끔하겠다

---
+++ 14890 경사로/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+class Program
+{
+    static int N, L, count;
+    static int[,] map;
+    static void Main()
+    {
+        string[] nl = Console.ReadLine().Split();
+        N = int.Parse(nl[0]);
+        L = int.Parse(nl[1]);
+        map = new int[N, N];
+        for (int i = 0; i < N; i++)
+        {
+            string[] row = Console.ReadLine().Split();
+            for (int j = 0; j < N; j++)
+            {
+                map[i, j] = int.Parse(row[j]);
+            }
+        }
+        count = N * 2;
+        // check row
+        int cntH = 0;
+        for (int i = 0; i < N; i++)
+        {
+            cntH = map[i, 0];
+            bool[] hill = new bool[N];
+            for(int j = 1; j < N; j++)
+            {
+                if(cntH != map[i, j])
+                {
+                    if (cntH + 1 == map[i, j])
+                    {
+                        if (j - L < 0)
+                        {
+                            j = N;
+                            count--;
+                            continue;
+                        }
+                        for (int l = 1; l <= L; l++)
+                        {
+                            if (hill[j - l])
+                            {
+                                j = N;
+                                l = L + 1;
+                            }
+                            else if (cntH != map[i, j - l])
+                            {
+                                j = N;
+                                l = L + 1;
+                            }
+                        }
+                        if (j == N)
+                        {
+                            count--;
+                            continue;
+                        }
+                        for (int l = 1; l <= L; l++)
+                            hill[j - l] = true;
+                    }
+                    else if (cntH - 1 == map[i, j])
+                    {
+                        if (j + L > N)
+                        {
+                            j = N;
+                            count--;
+                            continue;
+                        }
+                        for (int l = 0; l < L; l++)
+                        {
+                            if (map[i, j] != map[i, j + l])
+                            {
+                                j = N;
+                                l = L;
+                            }
+                        }
+                        if (j == N)
+                        {
+                            count--;
+                            continue;
+                        }
+                        for (int l = 0; l < L; l++)
+                            hill[j + l] = true;
+                    }
+                    else
+                    {
+                        j = N;
+                        count--;
+                    }
+                }
+                cntH = map[i, j];
+            }
+        }
+        Console.WriteLine(count);
+    }
+}
