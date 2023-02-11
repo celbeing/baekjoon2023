3b02ff9a46db5320a1be8ec0a5a8683b3b05dedf
celbeing <kimsd1983@naver.com>
2023-02-11 16:33:39 +0900

    23.02.11.
    O(3^n) 시간 넉넉하게 초과!

--- 2629 양팔저울/Program.cs
+++ 2629 양팔저울/Program.cs
@@ -1,37 +1,33 @@
 ﻿using System;
 using System.Linq;
+using System.Collections.Generic;
 class Program
 {
     static int n, t;
     static int[] weight, bead;
-    static int[] sum;
-    static bool[] measure = new bool[15001];
+    static List<int> measure = new List<int>();
     static void Main()
     {
         n = int.Parse(Console.ReadLine());
-        sum = new int[n];
         weight = Console.ReadLine().Split().Select(int.Parse).ToArray();
         t = int.Parse(Console.ReadLine());
         bead = Console.ReadLine().Split().Select(int.Parse).ToArray();
-        check(0);
+        measure.Add(0);
+        for(int i = 1; i <= n; i++)
+        {
+            for(int j = 0; j < measure.Count; j++)
+            {
+                int k = measure[j];
+                k -= weight[i - 1];
+                if(k > 0 && !measure.Contains(k))
+                    measure.Add(k);
+                k += weight[i - 1] * 2;
+                if (k > 0 && !measure.Contains(k))
+                    measure.Add(k);
+            }
+        }
         for(int i = 0; i < t - 1; i++)
-            Console.Write(measure[bead[i]] ? "Y " : "N ");
-        Console.Write(measure[bead[t - 1]] ? "Y" : "N");
-    }
-    static void check(int d)
-    {
-        if (d == n)
-        {
-            int index = 0;
-            for(int i = 0; i < n; i++)
-                index += sum[i] * weight[i];
-            if (index > 0) measure[index] = true;
-            return;
-        }
-        for(int i = -1; i < 2; i++)
-        {
-            sum[d] = i;
-            check(d + 1);
-        }
+            Console.Write(measure.Contains(bead[i]) ? "Y " : "N ");
+        Console.Write(measure.Contains(bead[t - 1]) ? "Y" : "N");
     }
 }
