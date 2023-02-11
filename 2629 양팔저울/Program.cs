0602d7e488e6ef97f3658e38e172cb3bc5228df1
celbeing <kimsd1983@naver.com>
2023-02-11 16:54:39 +0900

    23.02.11.
    재귀 잘 안되는 듯

--- 2629 양팔저울/Program.cs
+++ 2629 양팔저울/Program.cs
@@ -5,29 +5,29 @@ class Program
 {
     static int n, t;
     static int[] weight, bead;
-    static List<int> measure = new List<int>();
+    static bool[,] measure = new bool[31, 40001];
     static void Main()
     {
         n = int.Parse(Console.ReadLine());
         weight = Console.ReadLine().Split().Select(int.Parse).ToArray();
         t = int.Parse(Console.ReadLine());
         bead = Console.ReadLine().Split().Select(int.Parse).ToArray();
-        measure.Add(0);
-        for(int i = 1; i <= n; i++)
-        {
-            for(int j = 0; j < measure.Count; j++)
-            {
-                int k = measure[j];
-                k -= weight[i - 1];
-                if(k > 0 && !measure.Contains(k))
-                    measure.Add(k);
-                k += weight[i - 1] * 2;
-                if (k > 0 && !measure.Contains(k))
-                    measure.Add(k);
-            }
-        }
+        dp(0, 0);
+        for(int i = 0; i < t; i++)
+            for(int j = 0; j < n; j++)
+                if (measure[j, bead[i]]) measure[30, bead[i]] = true;
         for(int i = 0; i < t - 1; i++)
-            Console.Write(measure.Contains(bead[i]) ? "Y " : "N ");
-        Console.Write(measure.Contains(bead[t - 1]) ? "Y" : "N");
+            Console.Write(measure[30, bead[i]] ? "Y " : "N ");
+        Console.Write(measure[30, bead[t - 1]] ? "Y" : "N");
+    }
+    static void dp(int k, int w)
+    {
+        if (k == n || measure[k,w]) return;
+        measure[k,w] = true;
+        dp(k + 1, w + weight[k]);
+        dp(k + 1, w);
+        int absolute = w - weight[k];
+        if (absolute < 0) absolute *= -1;
+        dp(k + 1, absolute);
     }
 }
