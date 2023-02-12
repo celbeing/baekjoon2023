361506f7cf217c6442301f976579ef43101aa45a
celbeing <kimsd1983@naver.com>
2023-02-12 22:23:21 +0900

    23.02.12.
    음

---
+++ 14891 톱니바퀴/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static int K;
+    static bool[,] gear = new bool[4,8];
+    static void Main()
+    {
+        for(int i = 0; i < 4; i++)
+        {
+            string g = Console.ReadLine();
+            for (int j = 0; j < 8; j++)
+            {
+                if (g[j] == '0') gear[i, j] = false;
+                else gear[i, j] = true;
+            }
+        }
+
+    }
+    static void turn(int n, int d)
+    {
+        bool mem;
+        if (d == 1) mem = gear[n, 7];
+        else mem = gear[n, 0];
+        for(int i = 0; i < 8; i++)
+    }
+}
