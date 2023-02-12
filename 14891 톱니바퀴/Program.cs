e7411c0e1551c664ba058ab1a4421eefc60e656c
celbeing <kimsd1983@naver.com>
2023-02-12 23:06:25 +0900

    23.02.12.

--- 14891 톱니바퀴/Program.cs
+++ 14891 톱니바퀴/Program.cs
@@ -2,7 +2,9 @@
 
 class Program
 {
-    static int K;
+    static int K, G, score;
+    static int[] flag = { 0, 0, 0, 0 };
+    static int[] wise = new int[4];
     static bool[,] gear = new bool[4,8];
     static void Main()
     {
@@ -15,13 +17,35 @@ class Program
                 else gear[i, j] = true;
             }
         }
-
-    }
-    static void turn(int n, int d)
-    {
-        bool mem;
-        if (d == 1) mem = gear[n, 7];
-        else mem = gear[n, 0];
-        for(int i = 0; i < 8; i++)
+        K = int.Parse(Console.ReadLine());
+        for(int i = 0; i < K; i++)
+        {
+            string[] t = Console.ReadLine().Split();
+            G = int.Parse(t[0]) - 1;
+            for (int j = 0; j < 4; j++) wise[j] = 0;
+            wise[G] = int.Parse(t[1]);
+            for(int j = 0; G + j < 3; j++)
+            {
+                int l = flag[G + j] + 2; if (l >= 8) l -= 8;
+                int r = flag[G + j + 1] + 6; if (r >= 8) r -= 8;
+                if (gear[G + j, l] != gear[G + j + 1, r]) wise[G + j + 1] = wise[G + j] * -1;
+            }
+            for(int j = 0; G - j > 0; j++)
+            {
+                int l = flag[G - j - 1] + 2; if (l >= 8) l -= 8;
+                int r = flag[G - j] + 6; if (r >= 8) r -= 8;
+                if (gear[G - j, r] != gear[G - j - 1, l]) wise[G - j - 1] = wise[G - j] * -1;
+            }
+            for(int j = 0; j < 4; j++)
+            {
+                flag[j] -= wise[j];
+                if (flag[j] < 0) flag[j] += 8;
+                else if (flag[j] >= 8) flag[j] -= 8;
+            }
+        }
+        for(int i = 0; i < 4; i++)
+            if (gear[i, flag[i]])
+                score += (int)Math.Pow(2, i);
+        Console.WriteLine(score);
     }
 }
