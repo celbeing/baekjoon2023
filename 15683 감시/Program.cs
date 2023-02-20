45f33a831cd88fb5f96ac1c40699a9c077df45bb
celbeing <kimsd1983@naver.com>
2023-02-20 10:11:53 +0900

    23.02.20.
     잠시 생각 좀

---
+++ 15683 감시/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static int N, M;
+    static List<int[]> office = new List<int[]>();
+    static void Main()
+    {
+        string[] nm = Console.ReadLine().Split();
+        N = int.Parse(nm[0]); M = int.Parse(nm[1]);
+        for(int i = 0; i < N; i++)
+            office.Add(Console.ReadLine().Split().Select(int.Parse).ToArray());
+
+    }
+
+}
