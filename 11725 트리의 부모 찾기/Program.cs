66e22e4fb0ad7ac9c76c2e98563312e8a887766c
celbeing <kimsd1983@naver.com>
2023-09-16 15:30:39 +0900

    23.09.16.
    복귀전 시간초과 대실패

---
+++ 11725 트리의 부모 찾기/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+class program
+{
+    static int N, a, b;
+    static string[] input = new string[2];
+    static Queue<int> edge = new Queue<int>();
+    static void Main()
+    {
+        N = int.Parse(Console.ReadLine());
+        int[,] nodes = new int[N, 2];
+        int[] mother = new int[N];
+        for (int i = 0; i < N - 1; i++)
+        {
+            input = Console.ReadLine().Split();
+            nodes[i, 0] = int.Parse(input[0]) - 1;
+            nodes[i, 1] = int.Parse(input[1]) - 1;
+        }
+        edge.Enqueue(0);
+        while (edge.Count != 0)
+        {
+            for (int i = 0; i < N - 1; i++)
+            {
+                if (nodes[i, 0] == edge.Peek())
+                {
+                    mother[nodes[i, 1]] = edge.Peek();
+                    edge.Enqueue(nodes[i, 1]);
+                    nodes[i, 0] = -1; nodes[i, 1] = -1;
+                }
+                else if (nodes[i, 1] == edge.Peek())
+                {
+                    mother[nodes[i, 0]] = edge.Peek();
+                    edge.Enqueue(nodes[i, 0]);
+                    nodes[i, 0] = -1; nodes[i, 1] = -1;
+                }
+            }
+            edge.Dequeue();
+        }
+        for (int i = 1; i < N; i++)
+        {
+            mother[i]++;
+            Console.WriteLine(mother[i]);
+        }
+    }
+}
