61b29d3a7d95c4986f36f82aad1976d56f59075c
celbeing <kimsd1983@naver.com>
2023-10-01 15:54:19 +0900

    23.10.01.
    어...어떡..하..지...어...어떡..하..지...어...어떡..하..지...어...어떡..하..지...어...어떡..하..지...어...어떡..하..지...어...어떡..하..지...어...어떡..하..지...어...어떡..하..지...

---
+++ 18290/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class program
+{
+    static int N, M, K;
+    static int[,] set = new int[12, 12];
+    static Stack<int> select = new Stack<int>();
+
+    static void Main()
+    {
+        string[] input = Console.ReadLine().Split();
+        N = int.Parse(input[0]);
+        M = int.Parse(input[1]);
+        K = int.Parse(input[2]);
+    }
+
+    void BackTrack(int i, int d)
+    {
+        
+    }
+}
